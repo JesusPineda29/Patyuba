c466340ee81dac698429441b5e6d0932e5c8354d
JesusPineda29 <jesuspineda1429@gmail.com>
2025-08-06 12:53:47 -0600

    componente gallery responsivo y terminado

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -3,7 +3,7 @@ const Gallery = () => {
     <div className="min-h-screen bg-slate-200 p-4 md:p-8">
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4 auto-rows-min">
         {/* Elemento Negro */}
-        <div className="h-[300px] md:col-start-1 md:row-start-1 md:row-span-3 md:h-[753px] group cursor-pointer">
+        <div className="h-48 md:col-start-1 md:row-start-1 md:row-span-3 md:h-[753px] group cursor-pointer">
           <div className="relative w-full h-full rounded-xl overflow-hidden shadow-2xl transition-all duration-500 hover:scale-105 hover:-rotate-1 hover:shadow-3xl">
             <img src="/pizar.jpeg" alt="imagen" className="w-full h-full object-cover" />
             <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-30 transition-opacity duration-300"></div>
@@ -31,7 +31,7 @@ const Gallery = () => {
         </div>
 
         {/* Elemento Azul */}
-        <div className="h-80 md:col-start-2 md:col-span-2 group cursor-pointer">
+        <div className="h-48 md:h-80 md:col-start-2 md:col-span-2 group cursor-pointer">
           <div className="relative w-full h-full rounded-xl overflow-hidden shadow-2xl transition-all duration-500 hover:scale-105 hover:-rotate-1 hover:shadow-3xl">
             <img src="/patyuba.png" alt="imagen" className="w-full h-full object-cover" />
             <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-30 transition-opacity duration-300"></div>
@@ -39,7 +39,7 @@ const Gallery = () => {
         </div>
 
         {/* Elemento Naranja */}
-        <div className="h-[300px] md:col-start-4 md:row-start-2 md:row-span-2 md:h-[545px] group cursor-pointer">
+        <div className="h-48 md:col-start-4 md:row-start-2 md:row-span-2 md:h-[545px] group cursor-pointer">
           <div className="relative w-full h-full rounded-xl overflow-hidden shadow-2xl transition-all duration-500 hover:scale-105 hover:rotate-2 hover:shadow-3xl">
             <img src="/sombrasc.png" alt="imagen" className="w-full h-full object-cover" />
             <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-30 transition-opacity duration-300"></div>
